229e2b360033a72488280241fddf62342af98b50
SadiaAfroze97 <68560036+SadiaAfroze97@users.noreply.github.com>
2021-03-07 00:19:04 +0600

    abc

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 ###project start####
+
+from tkinter import *
+
+class BillingSft:
+    def __init__ (self,root):
+        self.root=root
+        self.root.geometry("1350x700+0+0")
+        self.root.title("ABC Fashion House")
+
+
+
+
+
+
+
+
+root=Tk()
+obj = BillingSft(root)
+root.mainloop()
